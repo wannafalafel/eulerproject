9a8255b4f3ce35315eaf5390bfa9b0e0219d08e3
wannafalafel <mr.tim.miller123@gmail.com>
2016-01-05 11:51:39 -0700

    Update problem_three.rb

--- problem_three.rb
+++ problem_three.rb
@@ -11,8 +11,6 @@ end
 
 @prime_numbers = find_primes(@to_check)
 
-print "These are the prime numbers that are smaller than 1000:'\n' #{@prime_numbers} '\n' '\n'"
-
 def find_prime_factors(numbers)
   numbers.each do |num|
     if
@@ -24,5 +22,3 @@ end
 
 find_prime_factors(@prime_numbers)
 print "These are the numbers that are the prime factors of 600851475143 that are under 1000. '\n' #{@prime_factors}"
-# I need to find all the prime factors in 600851475143
-# To do so, I need to divide every number by every other number up to itself
